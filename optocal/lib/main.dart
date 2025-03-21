517f25e387a282fadfb9d7d400e54502452fe536
AltairAG <aaguilarg2024@cic.ipn.mx>
2025-03-21 12:54:44 -0600

    actualizacion de codigo final funcional

--- optocal/lib/main.dart
+++ optocal/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:optocal/presentation/screens/optocal.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,48 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: const Color.fromRGBO(140, 23, 87, 1),
+        // Configuración de la fuente
+        fontFamily: 'Poppins', // Cambiar la fuente globalmente
+        textTheme: TextTheme(
+          // Cambiar los estilos de los textos para Material 3
+          displayLarge: TextStyle(
+            fontFamily: 'Poppins',
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+          displayMedium: TextStyle(
+            fontFamily: 'Poppins',
+            fontSize: 28,
+            fontWeight: FontWeight.w600,
+          ),
+          displaySmall: TextStyle(
+            fontFamily: 'Poppins',
+            fontSize: 24,
+            fontWeight: FontWeight.w500,
+          ),
+          bodyLarge: TextStyle(
+            fontFamily: 'Poppins',
+            fontSize: 18,
+            fontWeight: FontWeight.normal,
+          ),
+          bodyMedium: TextStyle(
+            fontFamily: 'Poppins',
+            fontSize: 16,
+            fontWeight: FontWeight.normal,
+          ),
+          bodySmall: TextStyle(
+            fontFamily: 'Poppins',
+            fontSize: 14,
+            fontWeight: FontWeight.normal,
+          ),
         ),
       ),
+      home: Optocal(),
     );
   }
 }
---
+++ optocal/lib/presentation/screens/optocal.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+
+class Optocal extends StatefulWidget {
+  const Optocal({super.key});
+
+  @override
+  OptocalState createState() => OptocalState();
+}
+
+class OptocalState extends State<Optocal> {
+  String selectedFrom = 'Snellen';
+  String selectedTo = 'Snellen';
+
+  final TextEditingController inputController = TextEditingController();
+  String result = '';
+
+  // Función de conversión
+  String convertValue(String from, String to, String input) {
+    try {
+      double value;
+
+      // Convertir el valor de entrada a un valor numérico base común: Decimal
+      switch (from) {
+        case 'Snellen':
+          List<String> parts = input.split('/');
+          if (parts.length != 2) return 'Formato Snellen inválido';
+          double num = double.parse(parts[0]);
+          double denom = double.parse(parts[1]);
+          value = num / denom;
+          break;
+        case 'Metros':
+          double metros = double.parse(input);
+          value = 6 / metros;
+          break;
+        case 'Decimal':
+          value = double.parse(input);
+          break;
+        case 'Mar':
+          value = 1 / double.parse(input);
+          break;
+        default:
+          return 'Conversión no soportada';
+      }
+
+      // Convertir de Decimal al formato destino
+      switch (to) {
+        case 'Snellen':
+          double snellenDenom = 20 / value;
+          return '20/${snellenDenom.toStringAsFixed(1)}';
+        case 'Metros':
+          double metros = 6 / (20 / value);
+          return '${metros.toStringAsFixed(2)} m';
+        case 'Decimal':
+          return value.toStringAsFixed(2);
+        case 'Mar':
+          double mar = 1 / value;
+          return mar.toStringAsFixed(2);
+        default:
+          return 'Conversión no soportada';
+      }
+    } catch (e) {
+      return 'Error: Entrada inválida';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> availableOptions = ['Snellen', 'Metros', 'Decimal', 'Mar'];
+    availableOptions.remove(selectedFrom);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Conversiones'),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: inputController,
+                decoration: InputDecoration(
+                  labelText: 'Ingrese valor',
+                  border: OutlineInputBorder(),
+                  contentPadding: EdgeInsets.all(15),
+                ),
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            SizedBox(height: 20),
+
+            Text('Convertir de:', style: TextStyle(fontSize: 18)),
+            SizedBox(height: 20),
+            SegmentedButton<String>(
+              segments: [
+                ButtonSegment(value: 'Snellen', label: Text('Snellen')),
+                ButtonSegment(value: 'Metros', label: Text('Metros')),
+                ButtonSegment(value: 'Decimal', label: Text('Decimal')),
+                ButtonSegment(value: 'Mar', label: Text('Mar')),
+              ],
+              selected: {selectedFrom},
+              onSelectionChanged: (Set<String> newSelection) {
+                setState(() {
+                  selectedFrom = newSelection.first;
+                  selectedTo = availableOptions.first;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+
+            Text('Convertir a:', style: TextStyle(fontSize: 18)),
+            SizedBox(height: 20),
+            SegmentedButton<String>(
+              segments:
+                  availableOptions
+                      .map(
+                        (option) =>
+                            ButtonSegment(value: option, label: Text(option)),
+                      )
+                      .toList(),
+              selected: {selectedTo},
+              onSelectionChanged: (Set<String> newSelection) {
+                setState(() {
+                  selectedTo = newSelection.first;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).colorScheme.primary,
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+              ),
+              onPressed: () {
+                setState(() {
+                  result = convertValue(
+                    selectedFrom,
+                    selectedTo,
+                    inputController.text.trim(),
+                  );
+                });
+              },
+              child: Text(
+                'Convertir',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: const Color.fromARGB(
+                    255,
+                    46,
+                    6,
+                    6,
+                  ), // <- Forzar texto blanco
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+
+            Text(
+              'Resultado: $result',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
